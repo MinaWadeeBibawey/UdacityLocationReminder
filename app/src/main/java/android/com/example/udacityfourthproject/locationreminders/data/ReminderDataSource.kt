ede1698c4db8c2aa6d128b89ff8668a3c9aa9aa0
Mina Wadee <mina.wadae@innovitics.com>
2023-02-16 12:20:56 +0200

    First commet

---
+++ app/src/main/java/android/com/example/udacityfourthproject/locationreminders/data/ReminderDataSource.kt
@@ -0,0 +1,14 @@
+package android.com.example.udacityfourthproject.locationreminders.data
+
+import android.com.example.udacityfourthproject.locationreminders.data.dto.ReminderDTO
+import android.com.example.udacityfourthproject.locationreminders.data.dto.Result
+
+/**
+ * Main entry point for accessing reminders data.
+ */
+interface ReminderDataSource {
+    suspend fun getReminders(): Result<List<ReminderDTO>>
+    suspend fun saveReminder(reminder: ReminderDTO)
+    suspend fun getReminder(id: String): Result<ReminderDTO>
+    suspend fun deleteAllReminders()
+}
---
+++ app/src/main/java/android/com/example/udacityfourthproject/locationreminders/savereminder/SaveReminderViewModel.kt
@@ -0,0 +1,82 @@
+package android.com.example.udacityfourthproject.locationreminders.savereminder
+
+import android.app.Application
+import android.com.example.udacityfourthproject.R
+import android.com.example.udacityfourthproject.base.BaseViewModel
+import android.com.example.udacityfourthproject.base.NavigationCommand
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.PointOfInterest
+import android.com.example.udacityfourthproject.locationreminders.data.ReminderDataSource
+import android.com.example.udacityfourthproject.locationreminders.data.dto.ReminderDTO
+import android.com.example.udacityfourthproject.locationreminders.reminderslist.ReminderDataItem
+import kotlinx.coroutines.launch
+
+class SaveReminderViewModel(val app: Application, val dataSource: ReminderDataSource) :
+    BaseViewModel(app) {
+    val reminderTitle = MutableLiveData<String>()
+    val reminderDescription = MutableLiveData<String>()
+    val reminderSelectedLocationStr = MutableLiveData<String>()
+    val selectedPOI = MutableLiveData<PointOfInterest>()
+    val latitude = MutableLiveData<Double>()
+    val longitude = MutableLiveData<Double>()
+
+    /**
+     * Clear the live data objects to start fresh next time the view model gets called
+     */
+    fun onClear() {
+        reminderTitle.value = ""
+        reminderDescription.value = ""
+        reminderSelectedLocationStr.value = ""
+        selectedPOI.value = null
+        latitude.value = 0.0
+        longitude.value = 0.0
+    }
+
+    /**
+     * Validate the entered data then saves the reminder data to the DataSource
+     */
+    fun validateAndSaveReminder(reminderData: ReminderDataItem) {
+        if (validateEnteredData(reminderData)) {
+            saveReminder(reminderData)
+        }
+    }
+
+    /**
+     * Save the reminder to the data source
+     */
+    fun saveReminder(reminderData: ReminderDataItem) {
+        showLoading.value = true
+        viewModelScope.launch {
+            dataSource.saveReminder(
+                ReminderDTO(
+                    reminderData.title,
+                    reminderData.description,
+                    reminderData.location,
+                    reminderData.latitude,
+                    reminderData.longitude,
+                    reminderData.id
+                )
+            )
+            showLoading.value = false
+            showToast.value = app.getString(R.string.reminder_saved)
+            navigationCommand.value = NavigationCommand.Back
+        }
+    }
+
+    /**
+     * Validate the entered data and show error to the user if there's any invalid data
+     */
+    fun validateEnteredData(reminderData: ReminderDataItem): Boolean {
+        if (reminderData.title.isNullOrEmpty()) {
+            showSnackBarInt.value = R.string.err_enter_title
+            return false
+        }
+
+        if (reminderData.location.isNullOrEmpty()) {
+            showSnackBarInt.value = R.string.err_select_location
+            return false
+        }
+        return true
+    }
+}
